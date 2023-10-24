a62ba7c1cd68582a4d2767911b68957c5769db13
grro <>
2023-10-24 06:46:54 +0200

    log output improved

--- smartmeter_webthing/meter.py
+++ smartmeter_webthing/meter.py
@@ -106,12 +106,12 @@ class ReconnectingSerialReader:
                     self.reader.start()
             except Exception as e:
                 self.__logger.warning("error occurred processing watchdog " + str(e))
-        self.__logger.warning("watchdog terminated")
+        self.__logger.info("watchdog terminated")
 
     def start(self):
         self.reader.start()
         Thread(target=self.__watchdog, daemon=True).start()
-        self.__logger.warning("watchdog started")
+        self.__logger.info("watchdog started")
 
     def close(self):
         self.is_running = False
